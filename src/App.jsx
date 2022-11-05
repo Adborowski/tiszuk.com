d098de8cab5d1947ea984cbb625abe803dc40b49
Adam Borowski <adborowski@gmail.com>
2022-11-05 16:24:45 +0100

    Made it more wacky

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,24 @@ import Frame from "./components/Frame/Frame";
 
 function App() {
   let frames = [];
-  for (let i = 1; i <= 9; i++) {
+  const frameCount = 1;
+  for (let i = 1; i <= frameCount; i++) {
     frames.push(<Frame key={i} />);
   }
 
   return (
     <div className={styles.App}>
+      <div className={styles.rings}>
+        <div className={styles.ring}></div>
+      </div>
+
+      <div className={styles.rings}>
+        <div className={styles.ring}></div>
+      </div>
+
+      <div className={styles.rings}>
+        <div className={styles.ring}></div>
+      </div>
       <div className={styles.frames}>{frames}</div>
     </div>
   );
