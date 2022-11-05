f20f0875f7c22f313439bcffd45a8d17f99b0b46
Adam Borowski <adborowski@gmail.com>
2022-11-05 14:52:45 +0100

    Added sound

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const sound = new Audio("/ae.mp3");
+
+  const frameClicked = (frame) => {
+    console.log("click");
+    setIsAnimating(true);
+    sound.play();
+  };
+
+  const animationEnded = (frame) => {
+    console.log("end");
+    setIsAnimating(false);
+  };
+
   return (
     <div className={styles.App}>
-      <div className={styles.imageFrame}></div>
+      <div
+        onClick={frameClicked}
+        onAnimationEnd={animationEnded}
+        className={`${styles.imageFrame} ${
+          isAnimating ? styles.animating : ""
+        }`}
+      ></div>
     </div>
   );
 }
